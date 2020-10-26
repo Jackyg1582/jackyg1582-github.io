a964d980a12dac92bfd483a2db6e1f670a97b9c4
Jackyg1582 <73482051+Jackyg1582@users.noreply.github.com>
2020-10-26 10:28:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Jacky Gee"
+let userName = "Jackyg1582";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
